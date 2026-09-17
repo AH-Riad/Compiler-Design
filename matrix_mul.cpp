2d4643e06941c12a5ef635223c1b72ff20e7670d
Md. Ahsanul Haque Riad <riadk910@gmail.com>
2026-09-17 08:09:42 +0600

    Update debugging of matrix mul

--- matrix_mul.cpp
+++ matrix_mul.cpp
@@ -3,41 +3,77 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int r1, c1, r2, c2;
 
-    int arr[n];
+    // Input rows and columns of first matrix
+    cout << "Enter rows and columns of the first matrix: ";
+    cin >> r1 >> c1;
 
-    // Input array
-    for(int i = 0; i < n; i++)
+    int A[10][10];
+
+    // Input elements of first matrix
+    cout << "Enter elements of the first matrix:" << endl;
+
+    for (int i = 0; i < r1; i++)
     {
-        cin >> arr[i];
+        for (int j = 0; j < c1; j++)
+        {
+            cin >> A[i][j];
+        }
     }
 
-    // Selection Sort
-    for(int i = 0; i < n - 1; i++)
-    {
-        int minIndex = i;
+    // Input rows and columns of second matrix
+    cout << "Enter rows and columns of the second matrix: ";
+    cin >> r2 >> c2;
 
-        // Find minimum element
-        for(int j = i + 1; j < n; j++)
+    int B[10][10];
+
+    // Input elements of second matrix
+    cout << "Enter elements of the second matrix:" << endl;
+
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
         {
-            if(arr[j] < arr[minIndex])
+            cin >> B[i][j];
+        }
+    }
+
+    // Check whether multiplication is possible
+    if (c1 != r2)
+    {
+        cout << "Matrix multiplication is not possible." << endl;
+        return 0;
+    }
+
+    // Result matrix
+    int result[10][10];
+
+    // Matrix multiplication
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            result[i][j] = 0;
+
+            for (int k = 0; k < c1; k++)
             {
-                minIndex = j;
+                result[i][j] += A[i][k] * B[k][j];
             }
         }
-
-        // Put minimum element at correct position
-        swap(arr[i], arr[minIndex]);
     }
 
-    // Print sorted array
-    cout << "Sorted Array: " << endl;
+    // Print result
+    cout << "\nResult Matrix:" << endl;
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < r1; i++)
     {
-        cout << arr[i] << " ";
+        for (int j = 0; j < c2; j++)
+        {
+            cout << result[i][j] << " ";
+        }
+
+        cout << endl;
     }
 
     return 0;
